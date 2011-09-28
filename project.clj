18fe00c36e7990b5ddf3a384b6c8a3b06744a61f
fogus <mefogus@gmail.com>
2011-09-28 08:38:52 -0400

    Upgrading Clojure dependency to v1.3.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject unk "0.9.2"
   :description "A memoization library for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [clache "0.6.1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
-                     [swank-clojure "1.3.1"]
+                     [swank-clojure "1.3.0-SNAPSHOT"]
                      [marginalia "0.3.2"]])
 
