68bdfc930bc1147790f679744326464381ec6acf
fogus <mefogus@gmail.com>
2011-10-27 13:23:34 -0400

    Bumped to clache v0.7.0 to gain additional features

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject unk "0.9.2"
+(defproject unk "0.9.3"
   :description "A memoization library for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clache "0.6.1"]]
+                 [clache "0.7.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.3.0-SNAPSHOT"]
